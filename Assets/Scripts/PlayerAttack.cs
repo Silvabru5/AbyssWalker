93a505bacc444670eba7a2f39cf9095adc460cbf
=Carey Cunningham <=cunnigc@gmail.com>
2025-09-21 12:55:20 -0400

    cleaned up PlayerAttack code

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,12 +1,10 @@
 using System.Collections;
-// using System.Numerics;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject basicAttackHitbox;   // the triangle hitbox for the basic attack
     public GameObject strongAttackHitbox;  // the rectangle hitbox for the strong attack
-//    public GameObject target; // attack target
 
     public float attackCooldown = 1f;      // how long between attacks
     public float hitboxShowTime = 0.2f;    // how long the hitbox stays active
@@ -16,7 +14,7 @@ public class PlayerAttack : MonoBehaviour
     private PlayerControl playerControl;   // reference to the movement script to get aim direction
     private AimingCursor aimingCursor;     // added to get aiming direction
     
-    private Animator animator; //to use animator
+    private Animator animator;             // to use animator
     private bool isFacingRight;
 
     void Start()
@@ -24,7 +22,7 @@ public class PlayerAttack : MonoBehaviour
         // get a reference to the player movement script
         playerControl = GetComponent<PlayerControl>();
         animator = GetComponentInParent<Animator>();
-        aimingCursor = GetComponent<AimingCursor>();
+        aimingCursor = GetComponent<AimingCursor>(); // used to determine player orientation and determine attack vectors
     }
 
     void Update()
@@ -32,54 +30,31 @@ public class PlayerAttack : MonoBehaviour
         // don't do anything if player can't attack or reference is missing
         if (!canAttack || playerControl == null || Time.timeScale == 0f) return;
 
-        // Carey mod to make the attack go in the direction faced
-        Vector2 aimDir = new Vector2(aimingCursor.direction.x, aimingCursor.direction.y);
-        Debug.Log(aimDir);
-
-        // left mouse button = basic attack (triangle)
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
-            //Tristan Mod
-            //getting where the player last faced
-            //Vector2 aimDir = playerControl.LastMoveDirection;
-
-
-            //using animation blend tree params to decide attack direction
+            Vector2 aimDir = new Vector2(aimingCursor.direction.x, aimingCursor.direction.y);
             animator.SetFloat("AttackHorizontal", aimDir.x);
             animator.SetFloat("AttackVertical", aimDir.y);
-
-            //trigger basic attack
-            animator.SetTrigger("BasicAttack");
-
-
-
-
-
-            StartCoroutine(PerformAttack(basicAttackHitbox, true));
-        }
-        // right mouse button = strong attack (rectangle)
-        else if (Input.GetMouseButtonDown(1))
-        {
-
-//            //getting where the player last faced
-//            Vector2 aimDir = playerControl.LastMoveDirection;
-
-            animator.SetFloat("Horizontal", aimDir.x);
-            animator.SetFloat("Vertical", aimDir.y);
-
-
-            animator.SetTrigger("StrongTrigger");
-            StartCoroutine(PerformAttack(strongAttackHitbox, false));
+            // left mouse button = basic attack (triangle)
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("BasicAttack");
+                StartCoroutine(PerformAttack(basicAttackHitbox, aimDir, true));
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                animator.SetTrigger("StrongTrigger");
+                StartCoroutine(PerformAttack(strongAttackHitbox, aimDir, false));
+            }
         }
     }
 
-    IEnumerator PerformAttack(GameObject hitbox, bool rotateWithDirection)
+    IEnumerator PerformAttack(GameObject hitbox, Vector2 aimDir, bool rotateWithDirection)
     {
         canAttack = false;
         SoundManager.PlaySound(SoundTypeEffects.PLAYER_BARBARIAN_ATTACK, 1);
 
         // get the direction the player is facing
-        Vector2 aimDir = new Vector2(aimingCursor.direction.x, aimingCursor.direction.y); // playerControl.LastMoveDirection;
         float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
 
         // move the hitbox in front of the player
