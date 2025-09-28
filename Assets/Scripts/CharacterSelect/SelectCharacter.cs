55b646acaca7a3faa7b87593a2bea54bbd0e6f07
TristanUng <ungtristan@gmail.com>
2025-09-28 12:48:32 -0400

    Character Select Screen WIP
    - added descriptions
    - added UI
    - added font
    - adjusted script to show proper descriptions depending on hero selected

--- Assets/Scripts/CharacterSelect/SelectCharacter.cs
+++ Assets/Scripts/CharacterSelect/SelectCharacter.cs
@@ -5,6 +5,21 @@ public class SelectCharacter : MonoBehaviour
     public GameObject character1;
     public GameObject character2;
 
+    public GameObject characterTitle1;
+    public GameObject characterTitle2;
+
+    public GameObject characterDescription1;
+    public GameObject characterDescription2;
+
+    public GameObject characterIcon1;
+    public GameObject characterIcon2;
+
+
+
+
+
+
+
     private int number = 0;
 
     void Start()
@@ -23,5 +38,19 @@ public class SelectCharacter : MonoBehaviour
     {
         character1.SetActive(number == 0);
         character2.SetActive(number == 1);
+
+        characterDescription1.SetActive(number == 0);
+        characterDescription2.SetActive(number == 1);
+
+        characterTitle1.SetActive(number == 0);
+        characterTitle2.SetActive(number == 1);
+
+        characterIcon1.SetActive(number == 0);
+        characterIcon2.SetActive(number == 1);
+
+
+
+
+
     }
 }
