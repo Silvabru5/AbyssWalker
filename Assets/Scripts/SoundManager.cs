8d2a4729ffd51916d4e1517cbeaf31e29fc6935b
=Carey Cunningham <=cunnigc@gmail.com>
2025-09-29 08:55:51 -0400

    sound effects integration started, much more implementation needed

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,35 +2,51 @@ using System.Collections;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
+// used this enum to define the different sound effect types
 public enum SoundTypeEffects
 {
-    GAME_LEVEL_ENTER,
-    PLAYER_BARBARIAN_TAKES_DAMAGE,
-    PLAYER_BARBARIAN_ATTACK,
-    PLAYER_BARBARIAN_SPECIAL_ATTACK,
-    PLAYER_BARBARIAN_DEATH,
-    ENEMY_ATTACK_SPIDER,
-    ENEMY_TAKES_DAMAGE_SPIDER,
-    ENEMY_DEATH_SPIDER,
-    ENEMY_ATTACK_SKELETON,
-    ENEMY_TAKES_DAMAGE_SKELETON,
-    ENEMY_DEATH_SKELETON,
-    ENEMY_ATTACK_ZOMBIE,
-    ENEMY_TAKES_DAMAGE_ZOMBIE,
-    ENEMY_DEATH_ZOMBIE,
-    BOSS_DOOR_OPENS,
-    BOSS_DOOR_ENTER,
-    BOSS_DOOR_BLOCKED
+    // Player Characters
+    WARRIOR_ATTACK, WARRIOR_SPECIAL_ATTACK, WARRIOR_DASH_PORTAL, WARRIOR_TAKES_DAMAGE, WARRIOR_DEATH,
+    MAGE_ATTACK, MAGE_SPECIAL_ATTACK, MAGE_DASH_PORTAL, MAGE_TAKES_DAMAGE, MAGE_DEATH,
+    // Enemies - Normal
+    BAT_ATTACK, BAT_TAKES_DAMAGE, BAT_DEATH,
+    BUFF_ZOMBIE_ATTACK, BUFF_ZOMBIE_TAKES_DAMAGE, BUFF_ZOMBIE_DEATH,
+    GHOUL_ATTACK, GHOUL_TAKES_DAMAGE, GHOUL_DEATH,
+    SKELETON_ATTACK, SKELETON_TAKES_DAMAGE, SKELETON_DEATH,
+    SPIDER_ATTACK, SPIDER_TAKES_DAMAGE, SPIDER_DEATH,
+    ZOMBIE_ATTACK, ZOMBIE_TAKES_DAMAGE, ZOMBIE_DEATH,
+    // Enemies - Bosses
+    NECROMANCER_TAKES_DAMAGE, NECROMANCER_DEATH,
+    NECROMANCER_VATS_TAKES_DAMAGE, NECROMANCER_VATS_DESTROYED,
+    NECROMANCER_MINION_ATTACK, NECROMANCER_MINION_TAKES_DAMAGE, NECROMANCER_MINION_DEATH,
+    NECROMANCER_PUFF_SMOKE_SPAWN,
+    NECROMANCER_SKULLS_ATTACK, NECROMANCER_SKULLS_TAKES_DAMAGE, NECROMANCER_SKULLS_DEATH,
+    VAMPIRE_ATTACK, VAMPIRE_SPECIAL_ATTACK, VAMPIRE_TAKES_DAMAGE, VAMPIRE_DEATH,
+    VAMPIRE_BLOOD_RAIN_SPAWN,
+    // Events
+    TITLE_SCREEN_LOAD,
+    CEMETERY_LOAD, CEMETERY_BOSS_LOAD,
+    CAVE_LOAD, CAVE_BOSS_LOAD,
+    LEVEL_UP,
+    SKILL_UP, SKILL_RESET,
+    //  Objects
+    CEMETERY_TOTEM_TAKES_DAMAGE, CEMETERY_TOTEM_DESTROY, CEMETERY_BOSS_DOOR_BLOCKED, CEMETERY_DOOR_UNBLOCKED, CEMETERY_DORR_ENTER,
+    CAVE_TOTEM_TAKES_DAMAGE, CAVE_TOTEM_DESTROY, CAVE_BOSS_DOOR_BLOCKED, CAVE_DOOR_UNBLOCKED, CAVE_DORR_ENTER,
+    SKILL_UP_VENDOR_INTERACTION
 }
 
+// used this enum to define the different background sound types
 public enum SoundTypeBackground
 {
-    BACKGROUND_TITLE,
-    BACKGROUND_GAME,
-    BACKGROUND_BOSS
+    MAIN_MENU,
+    HOME_BASE,
+    CEMETERY,
+    CEMETERY_BOSS,
+    CAVE,
+    CAVE_BOSS
 }
 
-// make an array to represent the ENUM of sounds, for each sound, make an array of possible clips to play
+// create a class with 2 variables to allow for the sound effects to have differering amounts of sound options per sound type
 [System.Serializable]
 public class SoundEffectEntry
 {
@@ -45,13 +61,15 @@ public class SoundManager : MonoBehaviour
     private static SoundManager instance;
 
     [Header("Background Music")]
-    [SerializeField] private AudioClip[] soundListBackground;
-    [SerializeField] private AudioSource backgroundMusicSource;
+    [SerializeField] private AudioClip[] soundListBackground;    // drop down populated by the enum above
+    [SerializeField] private AudioSource backgroundMusicSource;  // used this enum to define the different sound types
 
     [Header("Sound Effects")]
-    [SerializeField] private SoundEffectEntry[] soundEffects;
-    [SerializeField] private AudioSource soundEffectsSource;
+    [SerializeField] private SoundEffectEntry[] soundEffects;  // drop down populated by the enum above 
+    [SerializeField] private AudioSource soundEffectsSource;   // used this enum to define the different sound types
 
+
+    // create a persistant instance that is active whichever scene is active
     private void Awake()
     {
         DontDestroyOnLoad(this);
@@ -59,7 +77,10 @@ public class SoundManager : MonoBehaviour
         else Destroy(gameObject); // prevent duplicates
 
         // play title screen background music
-        PlayBackgroundMusic(SoundTypeBackground.BACKGROUND_TITLE);
+        PlayBackgroundMusic(SoundTypeBackground.MAIN_MENU);
+
+        // play the load game sound effect
+        PlaySound(SoundTypeEffects.TITLE_SCREEN_LOAD);
     }
 
 
@@ -69,6 +90,7 @@ public class SoundManager : MonoBehaviour
         // if the sound is to be a takes damage, attack sounds only play some of the time
         if (Regex.IsMatch(sound.ToString(), "(_TAKES_DAMAGE|_ATTACK)$") & Random.Range(0, 2) == 0) return;
 
+        // look through the sound effect lists for any categories that match the one passed in and play a random one from the list
         foreach (var entry in instance.soundEffects)
             if (entry.type == sound && entry.clips.Length > 0)
             {
