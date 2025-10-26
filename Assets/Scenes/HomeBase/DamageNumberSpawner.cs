e337c9efd4500ec097137747fa17feab50f56025
TristanUng <ungtristan@gmail.com>
2025-10-26 01:40:56 -0400

    Fixed Damage Number Bug
    Again

--- Assets/Scenes/HomeBase/DamageNumberSpawner.cs
+++ Assets/Scenes/HomeBase/DamageNumberSpawner.cs
@@ -10,7 +10,10 @@ public class DamageNumberSpawner : MonoBehaviour
     void Awake()
     {
         if (instance == null)
+        {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
         else
             Destroy(gameObject);
     }
